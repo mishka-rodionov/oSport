2df0da1721cb77c33684dc39fda70e504f2f8baa
mishka_rodionov <rodionov.mikhail.a@gmail.com>
2020-04-21 23:04:03 +0400

    addede view model to event calendar

--- app/src/main/java/com/rodionov/osport/app/di/module/viewModelModule.kt
+++ app/src/main/java/com/rodionov/osport/app/di/module/viewModelModule.kt
@@ -1,9 +1,11 @@
 package com.rodionov.osport.app.di.module
 
+import com.rodionov.osport.presentation.eventcalendar.EventCalendarViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
 //    viewModel { AuthViewModel(get(), get(), get()) }
 //    viewModel { (requestId: Int) -> DetailRequestViewModel(get(), requestId = requestId) }
+    viewModel { EventCalendarViewModel() }
 }
---
+++ app/src/main/java/com/rodionov/osport/presentation/eventcalendar/EventCalendarViewModel.kt
@@ -0,0 +1,7 @@
+package com.rodionov.osport.presentation.eventcalendar
+
+import com.rodionov.osport.app.platform.BaseViewModel
+
+class EventCalendarViewModel : BaseViewModel() {
+
+}
