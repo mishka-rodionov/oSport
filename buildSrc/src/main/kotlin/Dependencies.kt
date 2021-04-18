4c397588114727c0672e8362c71619621faba195
Михаил Родионов <rodionov.mikhail.a@gmail.com>
2021-04-18 23:19:41 +0400

    added build src directory for dependencies

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,35 @@
+object App {
+    const val applicationId = "com.rodionov.osport"
+}
+
+object Releases {
+    const val versionCode = 1
+    const val versionName = "1"
+}
+
+object DefaultConfig {
+    const val minSdk = 21
+    const val targetSdk = 30
+    const val compileSdk = 30
+    const val buildTools = "29.0.3"
+}
+
+object Versions {
+    const val gradle = "4.1.3"
+    const val lifecycle_extensions = "2.0.0"
+    const val room = "2.2.6"
+    const val kotlin = "1.4.32"
+    const val koin = "2.1.6"
+    const val navigation = "2.3.4"
+    const val preference_ktx = "1.1.1"
+    const val constraint_layout = "2.1.0-alpha2"
+    const val lifecycle_viewmodel_ktx = "2.3.0"
+    const val retrofit = "2.6.1"
+    const val converter_gson = "2.6.1"
+    const val stetho = "1.5.1"
+    const val gson = "2.8.6"
+    const val lifecycle_livedata_ktx = "2.2.0"
+    const val multidex = "2.0.1"
+    const val material = "1.3.0-alpha04"
+    const val appcompat = "1.2.0"
+}
