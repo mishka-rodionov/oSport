83d56fa2f3692bc2f54f6ebf47ccb2eb07325ad4
Михаил Родионов <rodionov.mikhail.a@gmail.com>
2021-06-20 23:57:15 +0400

    created static function for building database, added database and dao to database koin module

--- app/src/main/java/com/rodionov/osport/data/database/OSportDatabase.kt
+++ app/src/main/java/com/rodionov/osport/data/database/OSportDatabase.kt
@@ -1,8 +1,11 @@
 package com.rodionov.osport.data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.rodionov.osport.data.database.OSportDatabase.Companion.DATABASE_VERSION
+import com.rodionov.osport.data.database.dao.UserDao
 import com.rodionov.osport.data.database.entities.UserEntity
 
 @Database(
@@ -10,10 +13,17 @@ import com.rodionov.osport.data.database.entities.UserEntity
     version = DATABASE_VERSION,
     exportSchema = false
 )
-abstract class OSportDatabase: RoomDatabase() {
+abstract class OSportDatabase : RoomDatabase() {
+
+    abstract fun userDao(): UserDao
 
     companion object {
         const val DATABASE_VERSION = 1
+        private const val DATABASE_NAME = "OSportDatabase"
+        fun createDatabase(context: Context): OSportDatabase =
+            Room.databaseBuilder(context, OSportDatabase::class.java, DATABASE_NAME)
+                .fallbackToDestructiveMigration().build()
+
     }
 
 }
