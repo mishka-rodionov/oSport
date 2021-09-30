89e21d562e9ba8b1665c3fea919c45f5c67caa34
Михаил Родионов <rodionov.mikhail.a@gmail.com>
2021-09-30 23:48:28 +0400

    created core resources module, moved common recources to core-resources module, updated some dependencies for new modules

--- app/src/main/java/com/rodionov/osport/presentation/event_center/EventCenterFragment.kt
+++ app/src/main/java/com/rodionov/osport/presentation/event_center/EventCenterFragment.kt
@@ -1,11 +1,9 @@
 package com.rodionov.osport.presentation.event_center
 
 import androidx.navigation.fragment.findNavController
-import androidx.viewbinding.ViewBinding
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.rodionov.osport.R
 import com.rodionov.osport.app.platform.BaseFragment
-import com.rodionov.osport.databinding.FragmentAccountBinding
 import com.rodionov.osport.databinding.FragmentEventCenterBinding
 
 class EventCenterFragment: BaseFragment(R.layout.fragment_event_center) {
--- feature/event_creator/src/main/java/com/rodionov/event_creator/presentation/event_creator/EventCreatorFragment.kt
+++ feature/event_creator/src/main/java/com/rodionov/event_creator/presentation/event_creator/EventCreatorFragment.kt
@@ -1,9 +1,7 @@
 package com.rodionov.event_creator.presentation.event_creator
 
-import by.kirich1409.viewbindingdelegate.viewBinding
-import com.rodionov.osport.R
-import com.rodionov.osport.app.platform.BaseFragment
-import com.rodionov.osport.databinding.FragmentEventCreatorBinding
+import com.rodionov.event_creator.R
+import com.rodionov.event_creator.databinding.FragmentEventCreatorBinding
 
 class EventCreatorFragment: BaseFragment(R.layout.fragment_event_creator) {
 
